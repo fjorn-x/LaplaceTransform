5d14e4bf86ca23a7904b3e82eae6e447cda46f08
fjorn-x <66999659+fjorn-x@users.noreply.github.com>
2021-05-30 17:14:40 +0530

    Update func.py

--- func.py
+++ func.py
@@ -1,4 +1,4 @@
-import math
+from math import factorial,pow
 
 def constant(a,b) :
     num=str(a)
@@ -8,8 +8,8 @@ def constant(a,b) :
 def expo(a,b) :
     
     num=str(a*1) 
-    #den="s - " + str(b)
-    c=-abs(b)
+    
+    c=abs(b)
     if b<=0 :
         den="s + " + str(c)
     else :
@@ -19,52 +19,75 @@ def expo(a,b) :
     
 
 def tfunc(constant,b) :
-    fact=int(math.factorial(b))
+    fact=int(factorial(b))
     num=str(fact*constant)
     den='s^'+str(b+1)
     return num,den
 
 def sine(a,b) :
     num=str(a*b)
-    den="s^2 + " + str(int(math.pow(b,2)))
+    den="s^2 + " + str(int(pow(b,2)))
     return num,den
 
 def cos(a,b) :
     num=str(a*b)+"s"
-    den="s^2 + " + str(int(math.pow(b,2)))
+    den="s^2 + " + str(int(pow(b,2)))
     return num,den
 
 def expo_sin(a, b): 
     num=str(b)
-    den='(s - ' + str(a) + ')^2 + '+str(int(math.pow(b,2))) 
+    c=abs(a)
+    if a<=0:
+        den='(s + ' + str(c) + ')^2 + '+str(int(pow(b,2))) 
+    else:
+        den='(s - ' + str(c) + ')^2 + '+str(int(pow(b,2))) 
     return num,den
 
 def expo_cos(a, b): 
-    num='s - ' +str(a)
-    den='(s - ' + str(a) + ')^2 + '+str(int(math.pow(b,2)))
+    
+    c=abs(a)
+    if a<=0 :
+        num='s + ' +str(c)
+        den='(s + ' + str(c) + ')^2 + '+str(int(pow(b,2)))
+    else:
+        num='s - ' +str(c)
+        den='(s - ' + str(c) + ')^2 + '+str(int(pow(b,2)))
     return num,den
 
 def expo_sinh(a, b): 
     num=str(b)
-    den='(s - ' + str(a) + ')^2 - '+str(int(math.pow(b,2)))
+    c=abs(a)
+    if a<=0:
+        den='(s + ' + str(c) + ')^2 '+' - '+str(int(pow(b,2)))
+    else:
+        den='(s - ' + str(c) + ')^2 '+' - '+str(int(pow(b,2)))
     return  num,den
 
 def expo_cosh(a, b): 
-    num='s - '+str(a)
-    den='(s - ' + str(a) + ')^2 - '+str(int(math.pow(b,2))) 
+    c=abs(a)
+    if a<=0 :
+        num='s + '+str(c)
+        den='(s + ' + str(c) + ')^2 - '+str(int(pow(b,2))) 
+    else:
+        num='s - '+str(c)
+        den='(s - ' + str(c) + ')^2 - '+str(int(pow(b,2))) 
     return num,den
 
 def t_expo(n,a) :
-    num=str(int(math.factorial(n)))
-    den='(s - ' + str(a) +')^' +str(n+1)
+    num=str(int(factorial(n)))
+    c=abs(a)
+    if a<=0 :
+        den='(s + ' + str(c) +')^' +str(n+1)
+    else:
+        den='(s - ' + str(c) +')^' +str(n+1)
     return  num,den
 
 def sinh(a,b) :
     num=str(int(a*b))
-    den='s^2 - '+str(int(math.pow(b,2)))
+    den='s^2 - '+str(int(pow(b,2)))
     return num,den
 
 def cosh(a,b) :
     num=str(a)+'s'
-    den='(s^2 - '+str(int(math.pow(b,2)))+')'
+    den='(s^2 - '+str(int(pow(b,2)))+')'
     return num,den
